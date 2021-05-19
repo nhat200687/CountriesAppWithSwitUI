6a41575652f6b844db59b9aa8cc244c2bcf219e3
hatana <nhat200687@gmail.com>
2021-05-19 21:40:02 +0700

    Initial Commit

---
+++ Countries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countries
+//
+//  Created by Ha Thanh Nhat on 19/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Ha Thanh Nhat on 19/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
